71bcfd034619aab00b6ffeff5eb7ab6b54a2c333
George Kemp <gkemp@hubspot.com>
2019-12-03 11:29:59 +0000

    Correct Test Type

--- src/2019/3/index.test.js
+++ src/2019/3/index.test.js
@@ -18,7 +18,7 @@ describe('Part One', () => {
   });
 });
 
-describe('Part One', () => {
+describe('Part Two', () => {
   it('Produces Correct Distance for Samples', () => {
     expect(partTwo(`R75,D30,R83,U83,L12,D49,R71,U7,L72
     U62,R66,U55,R34,D71,R55,D58,R83`)).toBe(610);
