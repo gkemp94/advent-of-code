3247993130dbf3b6447ff53464ae2297a3cd9eca
George Kemp <gkemp@hubspot.com>
2019-12-06 11:14:59 +0000

    Update Formatting

--- src/2019/6/index.js
+++ src/2019/6/index.js
@@ -3,56 +3,70 @@ function Node(name) {
   this.parent = null;
   this.children = [];
   this.flatten = () => {
-    return this.children.reduce((curr, child) => [...curr, child, ...child.flatten()], []);
+    return this.children.reduce(
+      (curr, child) => [...curr, child, ...child.flatten()],
+      []
+    );
   };
   this.countOrbits = () => {
-    return this.flatten().length + this.children.reduce((curr, child) => curr + child.countOrbits(), 0);
-  }
-};
+    return (
+      this.flatten().length +
+      this.children.reduce((curr, child) => curr + child.countOrbits(), 0)
+    );
+  };
+}
 
 function Tree(rootNode) {
   this._root = rootNode;
   this.nodeDict = {
-    [rootNode.name]: rootNode,
+    [rootNode.name]: rootNode
   };
   this.addOrbit = (parent, child) => {
     this.nodeDict[parent] = this.nodeDict[parent] || new Node(parent);
     this.nodeDict[child] = this.nodeDict[child] || new Node(child);
     this.nodeDict[child].parent = this.nodeDict[parent];
     this.nodeDict[parent].children.push(this.nodeDict[child]);
-  }
+  };
   this.countOrbits = () => {
     return this._root.countOrbits();
-  }
+  };
 }
 
-export const createTree = (input) => {
-  const tree = new Tree(new Node('COM'));
-  input.split(`\n`)
-    .map(orbit => orbit.split(')'))
+export const createTree = input => {
+  const tree = new Tree(new Node("COM"));
+  input
+    .split(`\n`)
+    .map(orbit => orbit.split(")"))
     .forEach(([parent, child]) => {
       tree.addOrbit(parent, child);
-    })
+    });
   return tree;
-}
+};
 
-export const partOne = (input) => createTree(input).countOrbits();
+export const partOne = input => createTree(input).countOrbits();
 
-export const partTwo = (input) => {
+export const partTwo = input => {
   const tree = createTree(input);
   const visitedDict = {};
-  let paths = [tree.nodeDict['YOU'].parent]; // The Object "YOU" are orbiting
+  let paths = [tree.nodeDict["YOU"].parent];
   let i = 0;
   while (paths.length) {
-    paths.forEach(({ name }) => { visitedDict[name] = true; });
-    let newPaths = [];
-    paths.forEach((path) => {
-      newPaths = [...newPaths, ...[path.parent, ...path.children].filter((node) => node && !visitedDict[node.name])];
+    paths.forEach(({ name }) => {
+      visitedDict[name] = true;
     });
-    if(newPaths.find(({ name }) => name === 'SAN')) {
+    let newPaths = [];
+    paths.forEach(path => {
+      newPaths = [
+        ...newPaths,
+        ...[path.parent, ...path.children].filter(
+          node => node && !visitedDict[node.name]
+        )
+      ];
+    });
+    if (newPaths.find(({ name }) => name === "SAN")) {
       return i;
     }
     paths = newPaths;
     i++;
   }
-}
+};
