f7a0d455cd67ee1706132c3a87f498e94323b89c
George Kemp <gkemp@hubspot.com>
2020-12-01 08:05:01 -0700

    Solves Day One

--- src/days/one/one.spec.ts
+++ src/days/one/one.spec.ts
@@ -1,5 +1,30 @@
-describe('Day One', () => {
-  it('returns a positive result', () => {
-    expect(true).toBe(true);
+import { partOne, partTwo, processInput } from "./one";
+import fs from 'fs';
+import path from 'path';
+
+const SAMPLE_INPUT = `1721
+979
+366
+299
+675
+1456`;
+
+describe('Part One', () => {
+  it('returns 514579 for sample input.', () => {
+    expect(partOne(processInput(SAMPLE_INPUT))).toBe(514579);
+  });
+  it('returns correct result for puzzle input', () => {
+    const data = processInput(fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8'));
+    expect(partOne(data)).toBe(980499);
+  })
+});
+
+describe('Part Two', () => {
+  it('returns 514579 for sample input.', () => {
+    expect(partTwo(processInput(SAMPLE_INPUT))).toBe(241861950);
+  });
+  it('returns correct result for puzzle input', () => {
+    const data = processInput(fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8'));
+    expect(partTwo(data)).toBe(200637446);
   })
 });
---
+++ src/days/one/one.ts
@@ -0,0 +1,25 @@
+export const processInput = (input: string) => {
+  return input.split('\n').map(x => parseInt(x));
+};
+
+export const partOne = (input: number[]) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i; j < input.length; j++) {
+      if ((input[i] + input[j]) === 2020) {
+        return input[i] * input[j];
+      }
+    }
+  }
+};
+
+export const partTwo = (input: number[]) => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i; j < input.length; j++) {
+      for (let k = j; k < input.length; k++) {
+        if ((input[i] + input[j] + input[k]) === 2020) {
+          return input[i] * input[j] * input[k];
+        }
+      }
+    }
+  }
+};
