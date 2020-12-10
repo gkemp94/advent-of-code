46b85fcf39ab54699a89f32eded1a62ecfd7ccb8
George Kemp <gkemp@hubspot.com>
2020-12-10 10:50:24 -0700

    Remvoe Counter

--- src/utils/memo.ts
+++ src/utils/memo.ts
@@ -1,16 +1,10 @@
 export const memo = <T extends any[], J>(func: (...args: T) => J) => {
   const cache: Record<string, J> = {};
-  let saved = 0;
-  let run = 0;
   return (...args: T): J => {
     const key = JSON.stringify(args);
     if (!cache[key]) {
-      run++;
       cache[key] = func(...args);
-    } else {
-      saved++;
     }
-
     return cache[key];
   }
 };
