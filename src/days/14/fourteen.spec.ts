7eee3f002e7defb3beeb7bf0f5d95f31b58a0a0a
George Kemp <georgekemp94@gmail.com>
2020-12-20 09:56:45 -0700

    Solve Day 14 Part two

--- src/days/14/fourteen.spec.ts
+++ src/days/14/fourteen.spec.ts
@@ -5,7 +5,7 @@ const SAMPLE_INPUT = processInput(getFile(__dirname, 'sample_1.txt'));
 const SAMPLE_INPUT_2 = processInput(getFile(__dirname, 'sample_2.txt'));
 const PUZZLE_INPUT = processInput(getFile(__dirname, 'input.txt'));
 
-describe('Part One', () => {
+xdescribe('Part One', () => {
   it('returns 165 for sample input.', () => {
    expect(partOne(SAMPLE_INPUT)).toBe(165);
   });
@@ -15,31 +15,13 @@ describe('Part One', () => {
   });
 });
 
-xdescribe('Part Two', () => {
+describe('Part Two', () => {
   it('returns 1068781 for sample input.', () => {
-    expect(partTwo(SAMPLE_INPUT_2)).toBe("1068781");
-  });
-
-  /*
-  it('returns 1068781 for sample input.', () => {
-    expect(partTwo('17,x,13,19'.split(','))).toBe("3417");
-  });
-
-  it('returns 1068781 for sample input.', () => {
-    expect(partTwo('67,x,7,59,61'.split(','))).toBe("779210");
-  });
-
-  it('returns 1068781 for sample input.', () => {
-    expect(partTwo('67,7,x,59,61'.split(','))).toBe("1261476");
-  });
-
-  it('returns 1068781 for sample input.', () => {
-    expect(partTwo('1789,37,47,1889'.split(','))).toBe("1202161486");
+    expect(partTwo(SAMPLE_INPUT_2)).toBe(208);
   });
 
   it('returns correct result for puzzle input', () => {
-    const result = partTwo(PUZZLE_INPUT[1]);
-    expect(result).toBe("807435693182510");
+    const result = partTwo(PUZZLE_INPUT);
+    expect(result).toBe(3705162613854);
   });
-  */
 });
--- src/days/14/fourteen.ts
+++ src/days/14/fourteen.ts
@@ -29,18 +29,38 @@ export const partOne = (input: string[][]) => {
   return Object.keys(mem).reduce((prev: number, curr: string) => prev + mem[curr as any], 0);
 };
 
+export const getAllAddresses = (addressBin: string[], mask: string[]): string[][] => {
+  const result: string[] = [];
+  for (let i = 0; i < addressBin.length; i++) {
+    if (mask[i] === '1') {
+      result[i] = mask[i];
+    } else if (mask[i] === 'X') {
+      return getAllAddresses(addressBin.slice(i + 1, addressBin.length), mask.slice(i + 1, mask.length)).reduce<string[][]>((prev, x) => {
+         prev.push([...result, '1', ...x]);
+         prev.push([...result, '0', ...x]);
+         return prev;
+      }, []);
+    } else {
+      result.push(addressBin[i]);
+    }
+  }
+  return [result];
+}
+
 export const partTwo = (input: string[][]) => {
   const NUM_REGEX = /[0-9]+/;
   const mem: Record<number, number> = {};
-  let currMask;
+  let currMask: string[];
   for (let i = 0; i < input.length; i++) {
     if (input[i][0] === 'mask') {
-      currMask = input[i][1].split('').map(x => parseInt(x));
+      currMask = input[i][1].padStart(36, "0").split('');
     } else {
       const address = parseInt(input[i][0].match(NUM_REGEX)![0]);
-      const value = parseInt(input[i][1]);
-      const maskedValue = applyMask(value, currMask);
-      mem[address] = maskedValue;
+      const addressBin = address.toString(2).padStart(36, "0").split('');
+      const allAddresses = getAllAddresses(addressBin, currMask!).map(x => parseInt(x.join(''), 2));
+      allAddresses.forEach(address => {
+        mem[address] = parseInt(input[i][1]);
+      });
     }
   }
   return Object.keys(mem).reduce((prev: number, curr: string) => prev + mem[curr as any], 0);
